e896898f80ec1836fcb7f7976fac2f2b99f6fd27
galoz <gal.oz@powtoon.com>
2019-09-25 16:43:38 +0300

    initial broken save

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,23 @@
-extern crate aljabar;
-use aljabar::{matrix, vector, Vector};
-
-fn identity() -> Vector<u8, 4> {
-    let pixel = vector![0,255,255,15];
-    let kernel = matrix![
-    [0,0,0],
-    [0,1,0],
-    [0,0,0]];
-    pixel*kernel
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn identity_test(){
-        let result = identity();
-        assert_eq!(result, vector![0,255,255,15])
-    }
-}
+//use ndarray::{Array, Array3};
+//
+//fn rgb_identity(pixel_arr: &Array3<f32>, kernel: &Array3<f32>) -> f32 {
+//    let result_m = pixel_arr.dot(kernel);
+//    result_m[[2,2]]
+//}
+//
+//#[cfg(test)]
+//mod tests {
+//    use super::*;
+//    use ndarray::arr3;
+//
+//    #[test]
+//    fn identity_test(){
+//        let rgb = [4f32,255,20];
+//        let null = [1f32, 1, 1];
+//        let pixel_arr = arr3(&[&null, &rgb, &null]);
+//        let kernel = arr3(&[[0f32,0,0], [0f32,1,0], [0f32, 0, 0]]);
+//        let result = rgb_identity(&pixel_arr, &kernel);
+//        assert_eq!(result, rgb)
+//    }
+//}
+//
