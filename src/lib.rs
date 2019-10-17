031ffcf77bb320cbe7fc1e59da08d46b2fffe272
galoz <gal.oz@powtoon.com>
2019-10-17 11:04:54 +0300

    compiles and runs but images look funky

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 use image::{self, DynamicImage, GenericImageView, Rgba, Rgb, Luma, ImageBuffer, GenericImage};
 
-pub fn matched_addition_rgba_9 (results: [[u8; 4];9]) -> Vec<u8> {
+pub fn matched_addition_rgba_9(results: [[u8; 4];9]) -> Vec<u8> {
     let mut fin= Vec::with_capacity(4);
     for idx in 0..4 {
-        fin.push(results.to_vec().iter().fold(0, |acc, x| acc + x[idx]))
+        fin.push(results.to_vec().iter().fold(0, |acc: u8, x| acc.saturating_add(x[idx])))
     }
     fin
 }
 
-fn matched_multiplication_9(kernel: [u8;9], mut pixel_cell: [[u8; 4];9]) -> [[u8; 4];9] {
+fn matched_multiplication_9(kernel: [i8;9], mut pixel_cell: [[u8; 4];9]) -> [[u8; 4];9] {
     for y in 0..kernel.len() {
         let mut pixl = pixel_cell[y];
         let point_val = kernel[y];
         for idx in 0..4usize {
-            pixl[idx] = (pixl[idx]*point_val)/9
+            pixl[idx] = (((pixl[idx] as i16) *point_val as i16)/9) as u8
         }
         pixel_cell[y] = pixl
         }
@@ -22,10 +22,10 @@ fn matched_multiplication_9(kernel: [u8;9], mut pixel_cell: [[u8; 4];9]) -> [[u8
 
 fn pixel_cell_9(image: &DynamicImage, position: (u32, u32)) -> [[u8; 4];9]  {
     let mut cell: [[u8; 4];9] = Default::default();
-    for y in 0..3 + position.1{
-        for x in 0..3 + position.0{
+    for y in 0..3 {
+        for x in 0..3 {
             let idx = (x+y) as usize;
-            cell[idx] = match image.get_pixel(x, y) {
+            cell[idx] = match image.get_pixel(x+ position.0, y+position.1) {
                 Rgba(color) => color,
                 _ => panic!("problem processing pixel!")
             }
@@ -34,47 +34,43 @@ fn pixel_cell_9(image: &DynamicImage, position: (u32, u32)) -> [[u8; 4];9]  {
     cell
 }
 
-fn traverse_image_9(image: &DynamicImage, kernel: [u8;9]) -> DynamicImage {
+fn traverse_image_9(image: &DynamicImage, kernel: [i8;9]) -> DynamicImage {
     let mut copy = image.clone();
     let (w, h) = image.dimensions();
 
     for y in 0..h-3 {
         for x in 0..w-3 {
-            let pixle = copy.get_pixel_mut(x+1, y+1);
+            let pixle = copy.get_pixel(x+1, y+1);
             let mut cell = pixel_cell_9(image, (x, y));
             let result = matched_addition_rgba_9(matched_multiplication_9(kernel, cell));
-            *pixle = Rgba([result[0], result[1], result[2], result[3]])
+            copy.put_pixel( x+1, y+1,Rgba([result[0], result[1], result[2], result[3]]));
         }
     }
     copy
 }
 
-// move to ::new Logic
-//fn image_kernel_ops(kernel_size: u8, image_path: &str){
-//
-//    let kernel = DMatrix::from_vec(col as u8, col as u8, vector);
-//    let img: DynamicImage = image.open(image).unwrap();
-//    let (w, h) = image::image_dimensions(image_path).unwrap();
-//}
-//
-//fn kernel_proc<T, F>(vector: Vec<i8>, kernel_size: u8, image: &DynamicImage, pos: &(u32, u32)) -> F
-//    where T: Iterator<Item = i8>,
-//    F: Iterator<Item = u8> {
-//    let multiplied: Vec<T> = matched_multiplication(vector, kernel_size, image, pos);
-//    let added: F = matched_addition(multiplied, kernel_size);
-//}
+fn costume_filter(image: &DynamicImage, kernel: [i8;9]) -> DynamicImage  {
+    traverse_image_9(image, kernel)
+}
 
-//}
+fn gaussianish_filter(image: &DynamicImage) -> DynamicImage {
+    let kernel = [1, 2, 1, 2, 4, 2, 1, 2, 1];
+    traverse_image_9(image, kernel)
+}
+
+fn edge_detection(image: &DynamicImage) -> DynamicImage {
+    let kernel = [1, 1, 1, 1, -8, 1, 1, 1, 1];
+    traverse_image_9(image, kernel)
+}
 
 #[cfg(test)]
-#[allow(unused_imports)]
 mod tests {
     use super::*;
 
     #[test]
     fn identity_test(){
         let kernel = [
-            0u8, 0, 0,
+            0i8, 0, 0,
             0, 9, 0,
             0, 0, 0];
         let pixel_cell: [[u8; 4];9] = [
@@ -88,7 +84,12 @@ mod tests {
                    [0,0,0,0], [1,1,0,1], [0,0,0,0],
                    [0,0,0,0], [0,0,0,0], [0,0,0,0]]);
         assert_eq!(matched_addition_rgba_9(multiplied), vec!(1,1, 0,1));
-
+    }
+    #[test]
+    fn test_save_edge_detc() {
+        let img = image::open("src/img.jpg").expect("could not find file to open!");
+        let edged = edge_detection(&img);
+        edged.save("copy.jpg").expect("could not save the file");
     }
 }
 
