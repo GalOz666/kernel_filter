e206b5b026baa2c479dcd9b8054ff5e0ef27c265
galoz <gal.oz@powtoon.com>
2019-10-17 13:36:20 +0300

    more convoluted but more generic too!

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use image::{self, DynamicImage, GenericImageView, Rgba, Rgb, Luma, ImageBuffer, GenericImage};
 
-// make into generic iterator for rgba
-fn matched_addition_rgba_9(results: [[u8; 4];9]) -> Vec<u8> {
+// TODO: implement as traits to DynamicImage and GreyImage!!
+
+// make into generic iterator for rgba - cf. grey example
+fn matched_addition_rgba(results: &[[u8; 4]]) -> Vec<u8> {
     let mut fin= Vec::with_capacity(4);
     for idx in 0..4 {
         fin.push(results.to_vec().iter().fold(0, |acc: u8, x| acc.saturating_add(x[idx])))
@@ -9,18 +11,24 @@ fn matched_addition_rgba_9(results: [[u8; 4];9]) -> Vec<u8> {
     fin
 }
 
-fn matched_multiplication_9(kernel: [i8;9], mut pixel_cell: [[u8; 4];9]) -> [[u8; 4];9] {
+fn matched_addition_grey<T>(results: T) -> u8
+    where T: Iterator<Item = u8> {
+    unimplemented!()
+}
+
+fn matched_multiplication<'a>(kernel: &[i8], pixel_cell: &'a mut [[u8; 4]]) -> &'a [[u8; 4]] {
     for y in 0..kernel.len() {
         let mut pixl = pixel_cell[y];
         let point_val = kernel[y];
-        for idx in 0..4usize {
-            pixl[idx] = (((pixl[idx] as i16) *point_val as i16)/9) as u8
+        for idx in 0..pixel_cell[0].len() {
+            pixl[idx] = ((((pixl[idx] as i16) *point_val as i16)/9) as u8)
         }
         pixel_cell[y] = pixl
         }
     pixel_cell
 }
 
+// genericise for 9 / 25
 fn pixel_cell_9(image: &DynamicImage, position: (u32, u32)) -> [[u8; 4];9]  {
     let mut cell: [[u8; 4];9] = Default::default();
     for y in 0..3 {
@@ -33,36 +41,57 @@ fn pixel_cell_9(image: &DynamicImage, position: (u32, u32)) -> [[u8; 4];9]  {
         }
     }
     cell
+
+}fn pixel_cell_25(image: &DynamicImage, position: (u32, u32)) -> [[u8; 4];25]  {
+    let mut cell: [[u8; 4];25] = Default::default();
+    for y in 0..5 {
+        for x in 0..5 {
+            let idx = (x+y) as usize;
+            cell[idx] = match image.get_pixel(x+ position.0, y+position.1) {
+                Rgba(color) => color,
+                _ => panic!("problem processing pixel!")
+            }
+        }
+    }
+    cell
 }
 
-fn traverse_image_9(image: &DynamicImage, kernel: [i8;9]) -> DynamicImage {
+// TODO: genericise with kernel as slice and dispatch accordingly: either 25/9.
+pub fn traverse_image_9(image: &DynamicImage, kernel: &[i8]) -> DynamicImage {
     let mut copy = image.clone();
     let (w, h) = image.dimensions();
 
-    for y in 0..h-3 {
-        for x in 0..w-3 {
+    let pix_cell = match kernel.len() {
+        9 => Box::new(pixel_cell_9),
+        23 => Box::new(pixel_cell_9), // turn to 25!
+        _ => panic!("supported kernel size are only 9 (3*3) or 25 (5*5)!")
+    };
+
+    let limit = (kernel.len() as f64).sqrt() as u32;
+    for y in 0..h-limit {
+        for x in 0..w-limit {
             let pixle = copy.get_pixel(x+1, y+1);
-            let mut cell = pixel_cell_9(image, (x, y));
-            let result = matched_addition_rgba_9(matched_multiplication_9(kernel, cell));
+            let mut cell = pix_cell(image, (x, y));
+            let result = matched_addition_rgba(&matched_multiplication(kernel, &mut cell));
             copy.put_pixel( x+1, y+1,Rgba([result[0], result[1], result[2], result[3]]));
         }
     }
     copy
 }
 
-pub fn costume_filter(image: &DynamicImage, kernel: [i8;9]) -> DynamicImage  {
+pub fn costume_filter(image: &DynamicImage, kernel: &[i8]) -> DynamicImage  {
     traverse_image_9(image, kernel)
 }
 
-pub fn gaussianish_filter(image: &DynamicImage) -> DynamicImage {
+pub fn gaussian_filter(image: &DynamicImage) -> DynamicImage {
     let kernel = [1, 2, 1, 2, 4, 2, 1, 2, 1];
-    traverse_image_9(image, kernel)
+    traverse_image_9(image, &kernel)
 }
 
 pub fn edge_detection(image: &DynamicImage) -> DynamicImage {
     // create_grey scale
     let kernel = [1, 1, 1, 1, -8, 1, 1, 1, 1];
-    traverse_image_9(image, kernel)
+    traverse_image_9(image, &kernel)
     // otsu threshold
 }
 
@@ -76,22 +105,22 @@ mod tests {
             0i8, 0, 0,
             0, 9, 0,
             0, 0, 0];
-        let pixel_cell: [[u8; 4];9] = [
+        let mut pixel_cell: [[u8; 4];9] = [
             [1,2,3,4],[1,2,3,4], [1,253,3,4],
             [1,2,3,4], [1,1,0,1], [5,2,3,4],
             [1,2,3,4], [1,2,5,5], [1,2,3,4],
         ];
-        let multiplied = matched_multiplication_9(kernel, pixel_cell);
+        let multiplied = matched_multiplication(&kernel, &mut pixel_cell);
         assert_eq!(multiplied,
                    [[0u8,0,0,0],[0,0,0,0], [0,0,0,0],
                    [0,0,0,0], [1,1,0,1], [0,0,0,0],
                    [0,0,0,0], [0,0,0,0], [0,0,0,0]]);
-        assert_eq!(matched_addition_rgba_9(multiplied), vec!(1,1, 0,1));
+        assert_eq!(matched_addition_rgba(&multiplied), vec!(1,1, 0,1));
     }
     #[test]
     fn test_save_edge_detc() {
         let img = image::open("src/img.jpg").expect("could not find file to open!");
-        let edged = gaussianish_filter(&img);
+        let edged = gaussian_filter(&img);
         edged.save("copy.jpg").expect("could not save the file");
     }
 }
